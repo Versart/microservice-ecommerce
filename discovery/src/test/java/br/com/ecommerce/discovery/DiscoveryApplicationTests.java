cf9eda852066695696e9dcadb05ff68cf72303f9
Versart <wesleyversart@gmail.com>
2023-12-27 10:34:53 -0300

    subindo o codigo para o github

---
+++ discovery/src/test/java/br/com/ecommerce/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
